5d65167d5b718830cf98200b6fe7d80505355b03
mattia <mattia.porco981@gmail.com>
2020-10-14 11:15:56 +0200

    V 2.0

--- mao.h
+++ mao.h
@@ -4,31 +4,43 @@
 #include <string>
 #include <string.h>
 #include <iostream>
+#include <sstream>
+#include <algorithm>
+#include <iterator>
+#include <vector>
+#include <cstdlib>
 
 namespace mao {
     #define MAO_MAO "hello world"
+    #define MAOO_MAO_MAO 3
 
     #define maoi int
     #define mao8 int8_t
     #define mao16 int16_t
     #define mao64 int64_t
+    #define maool bool
 
     #define maos std::string
     #define maoc char
+    #define maoctor std::vector
 
     #define mao4 for
     #define mahile while
     #define maoo if
+    #define maoooo else
 
     #define maao return
     #define maoid void
+    #define mao_ const
     #define maaao using
     #define maooo namespace
+    #define _mao_ main
 
     /// maomao
     /// io
     #define maout std::cout
     #define maoin std::cin
+    #define maoinln std::getline
     #define maoend std::endl
 
 }
--- traduttore.cpp
+++ traduttore.cpp
@@ -2,7 +2,57 @@
 
 maaao maooo mao;
 
-maoi main(){
-    maout << "mao";
+maool maoMaoMaoc(maoc c){
+    maao c == '?' || c == '!' || c == ',' || c == '.' || c == ';' || c == ':';
+}
+
+maoctor<maos> maoMaos(mao_ maos &s, mao_ maoc &ma){
+    maoctor<maos> maomao = maoctor<maos>();
+    maos maaoo = "";
+    mao4(maoi i = 0; i < s.size(); i ++){
+        maool spc = maoMaoMaoc(s[i]);
+        maoo(spc) {
+            maos m;
+            m += s[i];
+            maomao.push_back(maaoo);
+            maomao.push_back(m);
+            maaoo = "";
+        } maoooo maoo(s[i] == ma){
+            maomao.push_back(maaoo);
+            maaoo = "";
+        } maoooo {
+            maaoo += s[i];
+        }
+    }
+    maoo(maaoo != "") maomao.push_back(maaoo);
+    maao maomao;
+}
+
+maos mamaoMao(mao_ maos &maaaooo){
+    maoo(maoMaoMaoc(maaaooo[0])) maao maaaooo + " ";
+    maoo(maaaooo.size() < MAOO_MAO_MAO) maao "mao ";
+    maos maomao = "";
+    maomao += "m";
+    maoi r = (rand() % 3) + 1;
+    mao4(maoi i = 0; i < r; i++) maomao += "a";
+    r = (rand() % 3) + 1;
+    mao4(maoi i = 0; i < r; i++) maomao += "o";
+    maao maomao += " ";
+}
+
+maos maoMaoo(mao_ maos &input){
+    maoctor<maos> mm = maoMaos(input, ' ');
+    maos maomao = "";
+    mao4(maos &s : mm){
+        maomao += mamaoMao(s);
+    }
+    maao maomao;
+}
+
+
+maoi _mao_(){
+    maos maoh;
+    maoinln(maoin, maoh);
+    maout << maoMaoo(maoh);
     maao 0;
 }
